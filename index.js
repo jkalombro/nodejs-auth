dc874b50b8949ed5e4cad480c41a09f78507a63b
jkninja238 <jkninja238@gmail.com>
2022-07-17 13:04:58 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import env from 'dotenv';
+
+import userRoutes from './src/routes/user.route.js';
+
+//configure env
+env.config();
+
+//configure app
+const app = express();
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+//ROUTES
+app.get('/', (req, res) => res.send('APP IS RUNNING..'));
+app.use('/user', userRoutes);
+
+const CONNECTION_URL = process.env.CONNECTION_URL;
+const PORT = process.env.PORT || 5000;
+
+//configure db
+mongoose
+    .connect(CONNECTION_URL)
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((err) => console.log(err.message));
